c1db98f7e5c47875a5be9a9b4b8d0564ea33e502
박소희 <sthgml@naver.com>
2025-03-27 08:21:41 +0900

    temp

--- src/HashRouter.js
+++ src/HashRouter.js
@@ -3,8 +3,8 @@ import Router from "./Router.js";
 export default class HashRouter extends Router {
   constructor(routes, loginStore) {
     super(routes, loginStore);
-    this.currentPath = window.location.hash.slice(1) || "/";
-    this.startDetectForHash();
+    // this.currentPath = window.location.hash.slice(1) || "/";
+    // this.startDetectForHash();
   }
 
   // override
